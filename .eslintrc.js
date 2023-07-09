34ccad3d9fc8456fd95b374a527ea0683762040f
Kamikaze <timo1322@gmail.com>
2023-07-09 08:08:12 +0200

    Configured eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,14 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
   ],
+  rules: {
+    "no-plusplus": "warn",
+    "no-restricted-syntax": "warn",
+    "no-underscore-dangle": "warn",
+    "no-await-in-loop": "warn",
+    "class-methods-use-this": "warn",
+    "linebreak-style": "off",
+    "no-restricted-properties": "warn",
+    "no-unused-vars": "warn"
+  }
 }
